62c55fe0af1227d6a5026c576286b4c9d1f43a65
Alexandre Neto <alexandrewillik@gmail.com>
2021-03-10 20:46:39 +0000

    Segunda versão
    Agora o programa organiza a lista em ordem crescente de pontuação ELO.
    Além disso, o filme vencedor aparece na pergunta seguinte.

--- 0001 elo rating.py
+++ 0001 elo rating.py
@@ -1,5 +1,6 @@
 from random import randint
 import re
+from operator import itemgetter #para ordenar a lista de filmes
 
 file_name = "elo.csv"
 
@@ -40,14 +41,16 @@ N = len(linhas_lidas)
 
 print("\tCOMPARADOR DE FILMES\t")
 print("\n")
+index_vencedor = -1 #usado para repetir o filme vencedor
 while (True):
     print("Compare os filmes abaixo.")
     print("Selecione o melhor filme entre as duas opções (A ou B)")
     print("Digite E no caso de empate")
     print("(Digite Q para sair)")
-
-    index_A = randint(1, N) - 1
-    index_B = index_A
+    if(index_vencedor == -1):
+        index_A = index_B = randint(1, N) - 1
+    else:
+        index_A = index_B = index_vencedor
     while (index_B == index_A):
         index_B = randint(1, N) - 1
 
@@ -75,14 +78,18 @@ while (True):
     elif (key in {'A', 'a'}):
         filmeA.elo_rating = update_rating("win",filmeA.elo_rating,filmeB.elo_rating,K)
         filmeB.elo_rating = update_rating("lose", filmeB.elo_rating, filmeA.elo_rating, K)
+        index_vencedor = filmeA.index
 
     elif (key in {'B', 'b'}):
         filmeA.elo_rating = update_rating("lose", filmeA.elo_rating, filmeB.elo_rating, K)
         filmeB.elo_rating = update_rating("win", filmeB.elo_rating, filmeA.elo_rating, K)
+        index_vencedor = filmeB.index
 
     elif (key in {'E', 'e'}):
         filmeA.elo_rating = update_rating("tie", filmeA.elo_rating, filmeB.elo_rating, K)
         filmeB.elo_rating = update_rating("tie", filmeB.elo_rating, filmeA.elo_rating, K)
+        index_vencedor = -1
+
     if (key in {'P', 'p'}):
         continue
     print("\n")
@@ -93,9 +100,24 @@ while (True):
     linhas_lidas[index_A][2] = str(filmeA.elo_rating)
     linhas_lidas[index_B][2] = str(filmeB.elo_rating)
 
+
+
 #Fim do laço de repetição
+
+
+#Reordena a lista
+for i in range(N):
+    linhas_lidas[i][2] = float(linhas_lidas[i][2])
+linhas_lidas = sorted(linhas_lidas,key=itemgetter(2),reverse=True) #ordena a lista de acordo com a pontuação ELO em ordem crescente
+for i in range(N):
+    linhas_lidas[i][0] = str(i+1)
+    linhas_lidas[i][2] = str(linhas_lidas[i][2])
+
+
+#Reconfigura a lista para o formato .csv
 linhas_lidas[:] = [";".join(linha) for linha in linhas_lidas]
 linhas_lidas[:] = [linha + "\n" for linha in linhas_lidas]
+
 print(linhas_lidas)
 #Salva o arquivo texto
 with open(file_name, 'w',encoding="utf8") as writer:
